3e9b01f3a7f07f86f1943fd5eb14ef6930353535
Laurent HINNEKINT <laurent.hinnekint@gmail.com>
2023-05-08 19:02:57 +0200

    Session du 08.05 dernier exercice manquant

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,74 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+
+function createListIngredients(arr) {
+
+    ingredients.forEach(function (ingredient) {                             //Je lance une boucle sur chaque ingrédient de la liste
+
+        if (ingredient.length === 0) return;                                //J'intterompt la fonction si la liste est vide
+
+        const li = document.createElement('li');                            // J'initialise une variable qui crée un élément LI
+        li.classList.add('flex-list-li');                                   // Je rajoute la classe "flex-list-li" à cet élément
+        document.getElementById('salad-ingredients').appendChild(li);       // Je transfert l'élément crée dans la nouvelle liste
+
+        const button = document.createElement('button');                    // J'initialise une variable qui crée un élément BUTTON
+        button.classList.add('flex-list-btn');                              // Je rajoute la classe "flex-list-bt" à cet élément
+        button.innerText += ingredient;                                     // Je copie le contenu de la liste d'ingrédients dans l'élément BUTTON
+        li.appendChild(button);                                             // Je transfert l'élément crée dans la nouvelle liste
+    })
+}
+
+
+
+createListIngredients(ingredients);
+
+
+
+function addIngredientToSalad() {
+    if (saladFinal.childElementCount >= 5) return;
+
+    const li = document.createElement('li');
+    li.classList.add('flex-list-li');
+    saladFinal.appendChild(li);
+
+    const button = document.createElement('button');
+    button.classList.add('flex-list-btn');
+    button.innerText += this.innerText;
+    li.appendChild(button);
+    countIngredients;
+}
+
+function countIngredients() {
+    document.getElementById("salad-count").innerText = saladFinal.childElementCount;
+}
+
+
+function removeIngredientFromSalad(event) {
+    event.target.remove()
+}
+
+
+
+
+const buttonSaladIng = document.querySelectorAll('#salad-ingredients .flex-list-btn');
+buttonSaladIng.forEach(function (button) {
+    button.addEventListener('click', addIngredientToSalad);
+});
+
+
+
+const buttonSaladFinal = document.querySelectorAll('#salad-final .flex-list-btn');
+console.log(buttonSaladFinal);
+buttonSaladFinal.forEach(function (button) {
+    button.addEventListener('click', removeIngredientFromSalad);
+    countIngredients();
+});
+
+
+
+const saladFinal = document.getElementById("salad-final");
+const saladIng = document.getElementById("salad-ingredients")
+
+
+
+
--- js/tabs.js
+++ js/tabs.js
@@ -1,23 +1,24 @@
-const underMenuItems = document.querySelectorAll('.undermenu-items');
 
-underMenuItems.forEach(function (item) {
-    item.addEventListener('mouseover', function (e) {
-        this.style.backgroundColor = "rgb(144, 68, 60)";
+const tabsLi = document.querySelectorAll('#tabs-links li a');
+const tabsArticles = document.querySelectorAll('#tabs-contents article');
+
+tabsLi.forEach(a => {
+  a.addEventListener('click', function () {
+    const activeTabLink = document.querySelector('#tabs-links li a.active');
+    activeTabLink.classList.remove('active');
+    this.classList.add('active');
+
+    const tabId = this.dataset.tab;
+    console.log(tabId);
+
+    tabsArticles.forEach(article => {
+      article.classList.remove('active');
+
+      if (article.id === tabId) {
+        article.classList.add('active');
+      }
     });
-    item.addEventListener('mouseout', function (e) {
-        this.style.backgroundColor = "rgb(21, 22, 23)";
-    });
+  });
 });
 
-const subMenus = document.querySelectorAll('.undermenu-right');
-console.log (menuItems);
 
-menuItems.forEach((menuItem, index) => {
-  menuItem.addEventListener('mouseenter', () => {
-    subMenus[index].classList.add('active');
-  });
-
-  menuItem.addEventListener('mouseleave', () => {
-    subMenus[index].classList.remove('active');
-  });
-});
